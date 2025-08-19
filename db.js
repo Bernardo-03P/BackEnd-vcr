a8cdc955fc02a2602e39151b60aaa5964109851e
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2025-08-19 17:35:35 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,22 +5,21 @@ const pool = new Pool({
     host: process.env.HOST_NAME,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    dialect: process.env.DB_DIALECT, // Esta linha não é usada pelo 'pg', mas pode ser mantida
+    dialect: process.env.DB_DIALECT, 
     port: process.env.PORT_NUMBER
 });
 
-// Apenas para verificar se a conexão com o banco de dados foi bem sucedida ao iniciar o servidor
+
 pool.connect((err, client, release) => {
     if (err) {
       return console.error('Erro ao conectar ao banco de dados:', err.stack);
     }
     console.log('Pool de conexão com o banco de dados criado com sucesso!');
-    // Libera o cliente de volta para o pool
+    
     release();
 });
 
-// Exportamos um objeto com uma função "query". 
-// O pool gerencia automaticamente a abertura e o fechamento das conexões para nós.
 module.exports = {
   query: (text, params) => pool.query(text, params),
+
 };
